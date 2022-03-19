5f37b33614588cce1eb828d6aabf11b309500f07
Diego Díaz <didimar2001@gmail.com>
2022-03-18 23:25:22 -0500

    layout se visualiza al comienzo de la aplicación. se añadió el layout como componente inical de la aplicación.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 
+import Layout from './Components/Layout/Layout';
+
 function App() {
   return (
     <>
@@ -33,51 +35,9 @@ function App() {
         </Switch>
       </BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <Layout>
+        <h2>MODIFICAR POR EL COMPOENENT HOME</h2>
+      </Layout>
     </div>
     </>
   );
