f12fdb103cc99a441996a398c8f523f412004dfe
Duhan Renteria <derh7280@hotmail.com>
2022-03-23 18:39:22 -0500

    Componente map en contacto

---
+++ src/Components/Contact/ContactMap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ContactMap.css';
+
+const ContactMap = ({ layoutVertical = false }) => {
+  return (
+    <>
+      <div
+        md={12}
+        className="bg-secondary text-white d-none d-sm-none d-md-block d-flex justify-content-center align-items-center map"
+      >
+        <div>Aquí pretendemos cargar un mapa</div>
+      </div>
+    </>
+  );
+};
+
+export default ContactMap;
