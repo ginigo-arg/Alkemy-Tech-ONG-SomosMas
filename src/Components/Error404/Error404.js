f98b28b7ddd35e87e93a5f97ef0c339c171ffd45
palaciosla <89703241+palaciosla@users.noreply.github.com>
2022-03-24 20:14:58 -0300

    Agrega layout publica a pagina 404

--- src/Components/Error404/Error404.js
+++ src/Components/Error404/Error404.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../../Routes/Layouts/Public';
+import Footer from '../Footer/Footer';
+import PublicFooter from '../Footer/PublicFooter';
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
-  )
-}
+    <Layout>
+      <div>Error404</div>
+    </Layout>
+  );
+};
 
-export default Error404
+export default Error404;
