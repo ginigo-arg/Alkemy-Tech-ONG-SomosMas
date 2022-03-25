6d131c61bbf4b0832c5cf3b0ea4567ce8e28acce
ginigo <gabrielinigo99@gmail.com>
2022-03-24 22:30:14 -0300

    Error al usar hook useLocation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import LayoutBackoffice from './Rutas/Layouts/LayoutBackoffice/LayoutBackoffice';
+import { Container } from 'react-bootstrap';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
 import NewsForm from './Components/News/NewsForm';
@@ -14,32 +17,62 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import About from './Components/About/Nosotros';
-import Layout from './Components/Layout/Layout';
+import { useLocation } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/Nosotros" component={About} />
+          {/* <Route path="/" exact component={} />  Esta ruta debe ser para el Home */}
         </Switch>
       </BrowserRouter>
-      <div className="App">
+
+      <Container className="App">
         <Layout>
-          <h2>MODIFICAR POR EL COMPOENENT HOME</h2>
+          <h2>MODIFICAR POR EL COMPONENT HOME</h2>
         </Layout>
-      </div>
+      </Container>
+      <Container fluid>
+        <LayoutBackoffice>
+          <BrowserRouter>
+            <Switch>
+              <Route
+                path="/backoffice/create-activity"
+                component={ActivitiesForm}
+              />
+              <Route
+                path="/backoffice/create-category"
+                component={CategoriesForm}
+              />
+              <Route path="/backoffice/create-news" component={NewsForm} />
+              <Route
+                path="/backoffice/backoffice/create-slide"
+                component={SlidesForm}
+              />
+              <Route
+                path="/backofice/create-testimonials"
+                component={TestimonialForm}
+              />
+              <Route path="/backoffice/create-user" component={UserForm} />
+              <Route path="/backoffice/create-member" component={MembersForm} />
+              <Route
+                path="/backoffice/create-project"
+                component={ProjectsForm}
+              />
+              <Route
+                path="/backoffice/school-campaign"
+                component={SchoolCampaign}
+              />
+              <Route
+                path="/backoffice/toys-campaign"
+                component={ToysCampaign}
+              />
+              <Route path="/backoffice/Nosotros" component={About} />
+            </Switch>
+          </BrowserRouter>
+        </LayoutBackoffice>
+      </Container>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import './assets/css/bootstrap-custom.css';
+import { Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
