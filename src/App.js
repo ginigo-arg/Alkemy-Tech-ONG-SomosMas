cf4720a21790de0ed094eb2259fe13f7efc36ccf
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-03-25 19:49:59 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -19,12 +19,8 @@ import ProjectsForm from './Components/Projects/ProjectsForm';
 import About from './Components/About/Nosotros';
 import Spinner from './Components/Spinner/Spinner';
 
-
-
-
 import Layout from './Routes/Layouts/Public';
 
-import Error404 from './Components/Error404/Error404';
 // IMPORTAR NUEVOS COMPONENTES DE WEB PUBLICA CON ESTE FORMATO:
 const Home = React.lazy(() => import('./Components/Home'));
 const Actividades = React.lazy(() =>
