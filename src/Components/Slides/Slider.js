c4e8cef8fa730c81b80d4a6e7a5bb1657d608ff5
palaciosla <89703241+palaciosla@users.noreply.github.com>
2022-03-27 18:12:48 -0300

    Crea archivo css para Slider

--- src/Components/Slides/Slider.js
+++ src/Components/Slides/Slider.js
@@ -1,41 +1,54 @@
-import React from "react";
-import { Carousel, CarouselItem } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { Carousel, CarouselItem } from 'react-bootstrap';
+import './Slider.css';
 
 const slides = [
   {
     id: 1,
-    name: "Prueba",
-    description: "Imagen de prueba slider",
+    name: 'Prueba',
+    description: 'Imagen de prueba slider',
     image:
-      "https://img.freepik.com/vector-gratis/ninos-felices-saltando-prado-verano_74855-5852.jpg?size=626&ext=jpg&ga=GA1.2.1586766005.1636675200",
+      'https://img.freepik.com/vector-gratis/ninos-felices-saltando-prado-verano_74855-5852.jpg?size=626&ext=jpg&ga=GA1.2.1586766005.1636675200',
   },
   {
     id: 2,
-    name: "Prueba2",
-    description: "Imagen de prueba slider",
+    name: 'Prueba2',
+    description: 'Imagen de prueba slider',
     image:
-      "https://img.freepik.com/vector-gratis/ninos-felices-saltando-prado-verano_74855-5852.jpg?size=626&ext=jpg&ga=GA1.2.1586766005.1636675200",
+      'https://img.freepik.com/vector-gratis/ninos-felices-saltando-prado-verano_74855-5852.jpg?size=626&ext=jpg&ga=GA1.2.1586766005.1636675200',
   },
   {
     id: 3,
-    name: "Prueba3",
-    description: "Imagen de prueba slider",
+    name: 'Prueba3',
+    description: 'Imagen de prueba slider',
     image:
-      "https://img.freepik.com/vector-gratis/ninos-felices-saltando-prado-verano_74855-5852.jpg?size=626&ext=jpg&ga=GA1.2.1586766005.1636675200",
+      'https://img.freepik.com/vector-gratis/ninos-felices-saltando-prado-verano_74855-5852.jpg?size=626&ext=jpg&ga=GA1.2.1586766005.1636675200',
   },
 ];
 
 const Slider = () => {
+  const [matches, setMatches] = useState(
+    window.matchMedia('(min-width: 768px)').matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia('(min-width: 768px)')
+      .addEventListener('change', (e) => setMatches(e.matches));
+  }, []);
+
   return (
     <Carousel>
       {slides &&
         slides.map((slide) => (
           <CarouselItem interval={5000} key={slide.name}>
             <img src={slide.image} alt={slide.name} className="w-100" />
-            <Carousel.Caption>
-              <h2 className='fw-bold text-uppercase fs-1'>{slide.name}</h2>
-              <p>{slide.description}</p>
-            </Carousel.Caption>
+            {matches && (
+              <Carousel.Caption className="caption">
+                <h2 className="fw-bold text-uppercase fs-1">{slide.name}</h2>
+                <p>{slide.description}</p>
+              </Carousel.Caption>
+            )}
           </CarouselItem>
         ))}
     </Carousel>
