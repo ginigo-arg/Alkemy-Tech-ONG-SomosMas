7c35abb8c65d071585efddf1b1d5219c90b1013a
Diego Díaz <didimar2001@gmail.com>
2022-03-29 17:20:30 -0500

    Petición GET reutilizable creada

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,16 +1,32 @@
 import axios from 'axios';
 
+// TODO: importar el método autorización de Nico
+
 const config = {
   headers: {
-    Group: 1,
-    // Aqui va el ID del equipo!!
+    Authorization: '',
+    // TODO: AGREGA LA AUTORIZACIÓN DE NICO
   },
 };
 
-const Get = () => {
-  axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-};
+export const GET_PRIVATE_API = async (url, id = null) => {
+  if (id) {
+    const data = await axios.get(url + id, config);
+    const response = JSON.parse(data);
 
-export default Get;
+    const res = response.data ?? new Error(response.message);
+
+    return res;
+  }
+
+  if (!id) {
+    const data = await axios.get(url, config);
+    const response = JSON.parse(data);
+
+    const res = response.data ?? new Error(response.message);
+
+    return res;
+  }
+
+  return new Error('Error en la petición');
+};
