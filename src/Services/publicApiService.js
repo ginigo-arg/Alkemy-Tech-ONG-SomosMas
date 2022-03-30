1a8fc5c624ca60565ac13a42edeee8e911c82754
palaciosla <89703241+palaciosla@users.noreply.github.com>
2022-03-30 17:33:52 -0300

    Arregla peticion Get

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,19 +2,19 @@ import axios from 'axios';
 
 const config = {
   headers: {
-    Group: 163, //Aqui va el ID del equipo!!
+    //group: 163, //Aqui va el ID del equipo!!
   },
 };
 
 const Get = async (URL, id = null) => {
   const url = id ? `${URL}/${id}` : URL;
-  console.log(url);
   try {
-    const res = await axios.get(url, config);
-    console.log(res);
+    const response = await axios(url, config);
+    return response
   } catch (error) {
     console.log(error);
   }
 };
+
 
 export default Get;
