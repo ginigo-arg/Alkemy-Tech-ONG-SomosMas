668eeef4cc5688e769147d04b29e187bc0e5dfc9
Duhan Renteria <derh7280@hotmail.com>
2022-03-29 20:36:20 -0500

    Metodo POST axios reutilizable Backoffice

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,37 @@
 import axios from 'axios';
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('TOKEN')}`,
+    Group: 163, //Aqui va el ID del equipo!!
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get('https://jsonplaceholder.typicode.com/users', config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+export const Post = async (URL, DATA, TOKEN) => {
+  const options = {
+    method: 'POST',
+    url: URL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+      Group: 163,
+    },
+    data: DATA,
+  };
+  try {
+    const response = await axios(options);
+    return response;
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+
+//export default Get
