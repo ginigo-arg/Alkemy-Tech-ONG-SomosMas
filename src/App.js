782d6b526a77a3d37c525824e10313770221860b
ginigo <gabrielinigo99@gmail.com>
2022-03-31 01:39:08 -0300

    actualizando app

--- src/App.js
+++ src/App.js
@@ -5,42 +5,40 @@ import React, { Suspense } from 'react';
 // import { Counter } from './features/counter/Counter';
 
 import './App.css';
-// import Layout from './Routes/Layouts/Public';
-// import LayoutBackoffice from './Routes/Layouts/LayoutBackoffice';
-// import ActivitiesForm from './Components/Activities/ActivitiesForm';
-// import CategoriesForm from './Components/Categories/CategoriesForm';
-// import NewsForm from './Components/News/NewsForm';
-// import SlidesForm from './Components/Slides/SlidesForm';
-// import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-// import UserForm from './Components/Users/UsersForm';
-// import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-// import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-// import MembersForm from './Components/Members/MembersForm';
-// import ProjectsForm from './Components/Projects/ProjectsForm';
-// import Spinner from './Components/Spinner/Spinner';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import Error404 from './Components/Error404/Error404';
-// import Detail from './Components/Activities/Detail/Detail';
-
-// IMPORTAR NUEVOS COMPONENTES DE WEB PUBLICA CON ESTE FORMATO::
-
-// const Home = React.lazy(() => import('./Components/Home'));
-// const Nosotros = React.lazy(() => import('./Components/About/Nosotros'));
-// const Actividades = React.lazy(() =>
-//   import('./Components/Activities/Actividades')
-// );
-// const IndexContact = React.lazy(() => import('./Components/Contact'));
-// const NewsList = React.lazy(() => import('./Components/News/NewsList'));
-// const NewDetail = React.lazy(() =>
-//   import('./Components/News/Detail/NewDetail')
-// );
+import Layout from './Routes/Layouts/Public';
+import LayoutBackoffice from './Routes/Layouts/LayoutBackoffice';
+import ActivitiesForm from './Components/Activities/ActivitiesForm';
+import CategoriesForm from './Components/Categories/CategoriesForm';
+import NewsForm from './Components/News/NewsForm';
+import SlidesForm from './Components/Slides/SlidesForm';
+import TestimonialForm from './Components/Testimonials/TestimonialsForm';
+import UserForm from './Components/Users/UsersForm';
+import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
+import MembersForm from './Components/Members/MembersForm';
+import ProjectsForm from './Components/Projects/ProjectsForm';
+import Spinner from './Components/Spinner/Spinner';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Error404 from './Components/Error404/Error404';
+import Detail from './Components/Activities/Detail/Detail';
+
+//IMPORTAR NUEVOS COMPONENTES DE WEB PUBLICA CON ESTE FORMATO::
+
+const Home = React.lazy(() => import('./Components/Home'));
+const Nosotros = React.lazy(() => import('./Components/About/Nosotros'));
+const Actividades = React.lazy(() =>
+  import('./Components/Activities/Actividades')
+);
+const IndexContact = React.lazy(() => import('./Components/Contact'));
+const NewsList = React.lazy(() => import('./Components/News/NewsList'));
+const NewDetail = React.lazy(() =>
+  import('./Components/News/Detail/NewDetail')
+);
 
 function App() {
   return (
     <div className="App">
-      <ToysCampaign />
-      {/* <Layout>
+      <Layout>
         <BrowserRouter>
           <Switch>
             Rutas para web pública
@@ -52,9 +50,8 @@ function App() {
               <Route path="/nosotros" component={Nosotros} />
               <Route path="/novedades/id" component={NewDetail} />
               <Route path="/novedades" component={NewsList} />
-            </Suspense> */}
-
-      {/* Rutas para el backoffice
+            </Suspense>
+            Rutas para el backoffice
             <Route path="/backoffice" exact component={LayoutBackoffice} />
             <Route
               path="/backoffice/create-activity"
@@ -78,18 +75,16 @@ function App() {
               component={SchoolCampaign}
             />
             <Route path="/backoffice/toys-campaign" component={ToysCampaign} />
-           
-
-            {/* Ruta error 404 */}
-      {/* <Route path="*" component={Error404} />
+            Ruta error 404
+            <Route path="*" component={Error404} />
           </Switch>
         </BrowserRouter>
       </Layout>
 
       <BrowserRouter>
-        <LayoutBackoffice> */}
-      {/* Rutas para el backoffice */}
-      {/* <Route path="/backoffice" exact component={LayoutBackoffice} />
+        <LayoutBackoffice>
+          {/* Rutas para el backoffice */}
+          <Route path="/backoffice" exact component={LayoutBackoffice} />
           <Route
             path="/backoffice/create-activity"
             component={ActivitiesForm}
@@ -112,9 +107,8 @@ function App() {
             component={SchoolCampaign}
           />
           <Route path="/backoffice/toys-campaign" component={ToysCampaign} />
-          
         </LayoutBackoffice>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   );
 }
