740863c8d7f4bf055f1a3f54eee300bc2da28049
ginigo <gabrielinigo99@gmail.com>
2022-03-30 21:31:06 -0300

    creando metodo put reutilizable

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,35 @@
 import axios from 'axios';
 
 const config = {
+  headers: {
+    Group: 01, //Aqui va el ID del equipo!!
+  },
+};
+
+export const Get = () => {
+  axios
+    .get('https://jsonplaceholder.typicode.com/users', config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+export const Put = async (id, Authorization, data) => {
+  const options = {
+    url: `${proccess.env.REACT_APP_API_CATEGORY}/${id}`,
+    method: 'PUT',
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+      Group: null,
+      Authorization,
+    },
+    body: { id, data },
+  };
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
+  try {
+    const resp = await axios(options);
+    return {
+      resp,
+    };
+  } catch (error) {
+    console.log('error catch:', error);
+  }
+};
