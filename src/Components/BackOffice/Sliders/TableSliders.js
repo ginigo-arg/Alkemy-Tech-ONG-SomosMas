47fe8d2494f5b2d838cb47be891feebaae03e009
palaciosla <89703241+palaciosla@users.noreply.github.com>
2022-03-31 21:13:45 -0300

    Arregla errores marcados por eslint

--- src/Components/BackOffice/Sliders/TableSliders.js
+++ src/Components/BackOffice/Sliders/TableSliders.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button, Table } from 'react-bootstrap';
-import './Sliders.css'
-//import { Link } from 'react-router-dom';
+import './Sliders.css';
 
 const slides = [
   {
