4ed6eb8fe2955452a562388141ad87825b739bc6
Diego Díaz <didimar2001@gmail.com>
2022-04-05 20:29:43 -0500

    Login Form y validación

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,33 +1,80 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import { Formik } from 'formik';
+import { Form, Container, Button } from 'react-bootstrap';
 
 const LoginForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    email: '',
-    password: '',
-  });
+  return <Container className='card p-0'>
+    <h4 className='card-header mb-4 p-4 p-sm-3'>Anywhere in your app!</h4>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        const { password } = values;
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      setInitialValues({ ...initialValues, email: e.target.value });
-    } if (e.target.name === 'password') {
-      setInitialValues({ ...initialValues, password: e.target.value });
-    }
-  };
+        const passValid = password.search(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/g);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-    localStorage.setItem('token', 'tokenValueExample');
-  };
+        if (!values.email) {
+          errors.email = 'Ingresar un correo';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Escribe un email válido.';
+        } else if (!values.password || passValid === -1) {
+          errors.password = 'Ingresar una contraseña válida.';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <Form className='rounded p-4 p-sm-3' noValidate onSubmit={handleSubmit}>
+          <Form.Group controlId="validationFormik01" className='mb-4'>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder='Escribe tu email'
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={!!errors.email}
+              isValid={values.email && !errors.email}
+            />
+            <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+          </Form.Group>
 
-  return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-      <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-      <button className="submit-btn" type="submit">Log In</button>
-    </form>
-  );
+          <Form.Group controlId="validationFormik01" className='mb-4'>
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder='Escribe tu contraseña'
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={!!errors.password}
+              isValid={values.password && !errors.password}
+            />
+            <Form.Control.Feedback type="invalid">{errors.password || touched.password}</Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit" disabled={isSubmitting}>
+            Ingresar
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  </Container>;
 };
 
 export default LoginForm;
