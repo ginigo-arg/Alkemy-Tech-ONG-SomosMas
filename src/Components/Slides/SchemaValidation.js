6e3d6d7345dd99afb346abdf8bd11a6e0c3be22d
ginigo-arg <gabrielinigo99@gmai.com>
2022-04-08 18:11:00 -0300

    mejorando validacion

--- src/Components/Slides/SchemaValidation.js
+++ src/Components/Slides/SchemaValidation.js
@@ -12,7 +12,7 @@ const checkFileFormat = img => {
 export const SchemaValidation = yup.object().shape({
   name: yup.string().required('el nombre el obligatorio').min(4, 'El nombre debe tener un minimo de 4 caracteres'),
   description: yup.string().required('La descripcion es obligatoria'),
-  order: yup.number().required('El ID es obligatorio').positive().integer(),
+  id: yup.number().required('El ID es obligatorio').typeError('Solo se admiten numeros'),
   img: yup.mixed().required('La imagen es obligatoria').test(
     'fileFormat',
     'Formato de imagen no válido',
