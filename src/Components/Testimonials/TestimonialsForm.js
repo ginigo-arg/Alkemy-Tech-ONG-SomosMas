20b5b7506a88668201cedcc15f172a9e8218d243
Palacios Leandro <palaciosleandroad@gmail.com>
2022-04-10 11:54:42 -0300

    Crea formulario para testimonios

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,119 +1,158 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, Form, Button, Container, Stack } from 'react-bootstrap';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import '../FormStyles.css';
+import '../TestimonialsForm.css';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { Oval } from 'react-loader-spinner';
+import FormImage from '../../assets/img/logo-somos-mas.png';
+import { useLocation } from 'react-router-dom';
+import {
+  GET_PRIVATE_API,
+  // Patch,
+  // POST_PRIVATE_API,
+} from '../../Services/privateApiService';
 
-const initialValues = {
-  name: '',
-  description: '',
-  image: [],
+const url = process.env.REACT_APP_API_TESTIMONIALS;
+
+const checkFileFormat = (photo) => {
+  if (photo) {
+    if (!['image/jpg', 'image/jpeg', 'image/png'].includes(photo.type)) {
+      return false;
+    }
+  }
+  return true;
 };
 
-const validate = (values, description) => {
-  const errors = {};
-  const { name, image } = values;
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(4, 'El nombre debe tener un mínimo de 4 caracteres')
+    .required('El nombre es obligatorio'),
+  description: Yup.string().required('La descripción es obligatoria'),
+  image: Yup.mixed()
+    .required('La foto es obligatoria')
+    .test('fileFormat', 'Formato de imagen no válido', checkFileFormat),
+});
 
-  if (!name) {
-    errors.name = 'El nombre es necesario.';
-  } else if (name.length < 4) {
-    errors.name = 'El nombre debe tener al menos 4 caracteres.';
-  }
+const TestimonialsForm = () => {
+  const location = useLocation();
+  const [edit, setEdit] = useState(false);
+  const [testimonials, setTestimonials] = useState(false);
 
-  if (!description) errors.description = 'La descripción es necesaria.';
+  useEffect(async () => {
+    if (location.state) {
+      setEdit(true);
+      const { data } = await GET_PRIVATE_API(url, location.state.id);
+      setTestimonials(data);
+    }
+  }, []);
 
-  if (!image) {
-    errors.image = 'La imagen es necesaria.';
-  } else if (!(['image/jpg', 'image/png', 'image/jpeg'].includes(
-    image.type))) {
-    errors.image = 'La imagen debe ser en formato JPG o PNG';
-  }
-
-  return errors;
-};
-
-const TestimonialForm = ({
-  toEdit = {
-    name: 'dsdas',
-    description: 'sdsadas',
-    image: [],
-  },
-}) => {
-  const [description, setDescription] = useState(toEdit ? toEdit.description : '');
-  const { errors, isSubmitting, handleSubmit, values, setFieldValue, handleChange, handleBlur } = useFormik({
+  const {
+    errors,
+    isSubmitting,
+    setFieldValue,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+  } = useFormik({
     validateOnBlur: true,
     validateOnChange: false,
-    initialValues: toEdit || initialValues,
-    validate: () => validate(values, description),
-    onSubmit: (values) => {
-      if (!description) console.log('no hay descripcion');
-      const dataForm = {
-        ...values,
-        description,
-      };
-      console.log(dataForm);
-      if (toEdit) {
-      // peticion patch
-        console.log('patch');
+    initialValues: {
+      name: testimonials.name || '',
+      description: testimonials.description || '',
+      image: testimonials.image || '',
+    },
+    validationSchema,
+    onSubmit: (values, { setSubmitting }) => {
+      if (!edit) {
+        // Función POST
+        console.log(values);
+        // POST_PRIVATE_API(url, values);
+        setSubmitting(false);
       } else {
-        // peticion post
-        // POST_PRIVATE_API(`${process.env.REACT_APP_API_TESTIMONIOS_POST}/create`, dataForm);
-        console.log('post');
+        // Función PUT
+        console.log(values);
+        // Patch(url, location.state.id, values);
+        setSubmitting(false);
       }
     },
   });
 
   return (
     <>
-      <form onSubmit={handleSubmit} className='my-4'>
-        <Container className='container-sm rounded-2 p-4 d-flex flex-column gap-2 shadow ' style={{ maxWidth: '700px', backgroundColor: '#f9fafb' }}>
-          <Stack className='align-items-center text-center'>
-            <h4 style={{ color: 'var(--bs-danger)' }}>{`Formulario para ${toEdit ? 'editar' : 'crear'} testimonios`}</h4>
-          </Stack>
-          <Form.Control
-            id="name"
-            name="name"
-            label="name"
-            placeholder='Nombre'
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {errors.name && (<Alert variant="danger" className='p-1 m-0'>
-            <p className='my-0 mx-2 fw-bolder'>{errors.name}</p>
-          </Alert>)}
-          <CKEditor editor={ClassicEditor} data={description} config={{ placeholder: 'Descripción' }} onChange={(e, editor) => {
-            const description = editor.getData();
-            setDescription(description);
-          }} />
-          {errors.description && (<Alert variant="danger" className='p-1 m-0'>
-            <p className='my-0 mx-2 fw-bolder'>{errors.description}</p>
-          </Alert>)}
-          <Form.Control
-            type="file"
-            accept='image/*'
-            id="image"
-            name="image"
-            onChange={(e) => {
-              setFieldValue('image', e.currentTarget.files[0]);
-            }}
-            onBlur={handleBlur}
-          />
-          {errors.image && (<Alert variant="danger" className='p-1 m-0'>
-            <p className='my-0 mx-2 fw-bolder'>{errors.image}</p>
-          </Alert>)}
-          <Stack className='align-items-end'>
-            <Button type='submit' variant="info" disabled={isSubmitting} className='w-25 d-flex justify-content-center fw-bolder' style={{ color: 'white' }}>{isSubmitting
-              ? (<Oval height={30} width={30} strokeWidth={5} color="red" secondaryColor='red'
-              />)
-              : 'Enviar'}</Button>
-          </Stack>
-        </Container>
-      </form>
+      <div className="container-all">
+        <div className="shadow">
+          <div className="container-logo pt-2 ">
+            <img src={FormImage} alt="logo" className="mx-auto d-block" />
+          </div>
+          <Container className="container-fluid rounded-2 d-flex flex-column gap-2 container-form ">
+            <form onSubmit={handleSubmit} className="my-4 formulario">
+              <Stack className="align-items-center text-center">
+                <h4 style={{ color: 'var(--bs-danger)' }}>
+                  {`Formulario para ${edit ? 'editar' : 'crear'} testimonios`}
+                </h4>
+              </Stack>
+              <Form.Control
+                id="name"
+                name="name"
+                label="name"
+                placeholder="Nombre"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.name && (
+                <Alert variant="danger" className="p-1 m-0">
+                  <p className="my-0 mx-2 fw-bolder">{errors.name}</p>
+                </Alert>
+              )}
+              <CKEditor
+                editor={ClassicEditor}
+                data={values.description}
+                config={{ placeholder: 'Descripción' }}
+                onChange={(e, editor) =>
+                  setFieldValue('description', editor.getData())
+                }
+              />
+              {errors.description && (
+                <Alert variant="danger" className="p-1 m-0">
+                  <p className="my-0 mx-2 fw-bolder">{errors.description}</p>
+                </Alert>
+              )}
+              <Form.Control
+                type="file"
+                accept="image/*"
+                id="image"
+                name="image"
+                onChange={(e) =>
+                  setFieldValue('photo', e.currentTarget.files[0])
+                }
+                onBlur={handleBlur}
+              />
+              {errors.image && (
+                <Alert variant="danger" className="p-1 m-0">
+                  <p className="my-0 mx-2 fw-bolder">{errors.image}</p>
+                </Alert>
+              )}
+              <Stack className="align-items-end">
+                <Button
+                  type="submit"
+                  variant="info"
+                  disabled={isSubmitting}
+                  className="w-25 d-flex justify-content-center fw-bolder"
+                  style={{ color: 'white' }}
+                >
+                  {isSubmitting ? <Oval /> : 'Enviar'}
+                </Button>
+              </Stack>
+            </form>
+          </Container>
+        </div>
+      </div>
     </>
   );
 };
 
-export default TestimonialForm;
+export default TestimonialsForm;
