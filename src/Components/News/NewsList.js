d840a5b72be18632defb9c6c4cff84f68124a188
Nicolás Corradini <nicocorradini471@gmail.com>
2022-04-14 11:24:15 -0300

    Added news request

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,38 +1,49 @@
 import '../CardListStyles.css';
-import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Container, Spinner } from 'react-bootstrap';
+import { getNews } from '../../Services/NewsService';
 import SectionTitles from '../SectionTitles/SectionTitles';
 import Card from '../Card/Card';
+import { useHistory } from 'react-router-dom';
 
-// Array para simular respuesta de Api
-const newMocks = [
-  { id: 1, name: 'title', description: 'description' },
-  { id: 2, name: 'title', description: 'description' },
-  { id: 3, name: 'title', description: 'description' },
-];
+const NewsList = () => {
+  const [news, setNews] = useState(false);
+  const history = useHistory();
 
-const NewsList = ({ news = newMocks }) => {
+  useEffect(async () => {
+    const res = await getNews();
+    setNews(res);
+  }, []);
+
   return (
     <>
       <SectionTitles title="Novedades" />
-      <Container>
-        <ul className="my-5 p-0 d-flex flex-wrap">
-          {news.length > 0
-            ? (
-              news.map((element) => {
-                return (
-                  <Card
-                    key={element.title}
-                    title={element.title}
-                    description={element.description}
-                  />
-                );
-              })
-            )
-            : (
-              <p className="w-100 my-5 text-center display-6">No hay novedades</p>
-            )}
-        </ul>
-      </Container>
+      {!news
+        ? <Spinner />
+        : <Container>
+          <ul className="my-5 p-0 d-flex flex-wrap">
+            {news.length > 0
+              ? (
+                news.map((element) => {
+                  return (
+                    <div
+                      key={element.id}
+                      onClick={() => history.push(`/novedades/${element.id}`)}
+                    >
+                      <Card
+                        title={element.name}
+                        image={element.image}
+                      />
+                    </div>
+                  );
+                })
+              )
+              : (
+                <p className="w-100 my-5 text-center display-6">No hay novedades</p>
+              )}
+          </ul>
+        </Container>
+      }
     </>
   );
 };
