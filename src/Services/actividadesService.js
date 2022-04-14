2375f2100da9a36df13c346ae38b37a2d4529b23
Horuuuu <h2.globo@gmail.com>
2022-04-14 14:00:39 -0300

    se agregan alerta de errores en actividadesservice

--- src/Services/actividadesService.js
+++ src/Services/actividadesService.js
@@ -1,3 +1,4 @@
+// import { Get, Post } from './publicApiService';
 import {
   DELETE_PRIVATE_API,
   GET_PRIVATE_API,
@@ -6,8 +7,58 @@ import {
   PUT_PRIVATE_API,
 } from './privateApiService';
 
+import { alertService } from './alertService';
+
 const url = process.env.REACT_APP_API_ACTIVITIES;
 
+const TYPE = 'Error';
+const MESSAGE_GET = 'Lo sentimos, hubo un error al obtener la/las actividades.';
+const MESSAGE_PUT = 'Lo sentimos, hubo un error al editar la/las actividades.';
+const MESSAGE_PATCH = 'Lo sentimos, hubo un error al editar la actividad.';
+const MESSAGE_POST = 'Lo sentimos, hubo un error al agregar una nueva actividad.';
+const MESSAGE_DELETE = 'Lo sentimos, hubo un error al eliminar la actividad.';
+
+export const getActivities = async (id = null) => {
+  try {
+    return await GET_PRIVATE_API(url, id);
+  } catch (error) {
+    alertService(TYPE, MESSAGE_GET);
+  }
+};
+
+export const putActivities = async (id, data) => {
+  try {
+    return await PUT_PRIVATE_API(url, id, data);
+  } catch (error) {
+    alertService(TYPE, MESSAGE_PUT);
+  }
+};
+
+export const patchActivities = async (id, data) => {
+  try {
+    return await PATCH_PRIVATE_API(url, id, data);
+  } catch (error) {
+    alertService(TYPE, MESSAGE_PATCH);
+  }
+};
+
+export const postActivities = async (data) => {
+  try {
+    return await POST_PRIVATE_API(url, data);
+  } catch (error) {
+    alertService(TYPE, MESSAGE_POST);
+  }
+};
+
+export const deleteActivities = async (id) => {
+  try {
+    return await DELETE_PRIVATE_API(url, id);
+  } catch (error) {
+    alertService(TYPE, MESSAGE_DELETE);
+  }
+};
+
+/*
 export const getActivities = (id = null) => {
   const res = GET_PRIVATE_API(url, id);
   return res.data;
@@ -28,3 +79,4 @@ export const postActivities = (data) => {
 export const deleteActivities = (id) => {
   return DELETE_PRIVATE_API(url, id);
 };
+*/
