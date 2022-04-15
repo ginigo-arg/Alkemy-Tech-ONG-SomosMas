2a98409b7050310cba604e5688817cfda82517b4
ginigo <gabrielinigo99@gmail.com>
2022-04-15 15:22:15 -0300

    Terminando actions

--- src/redux/actividades/actions.js
+++ src/redux/actividades/actions.js
@@ -1,16 +1,65 @@
-import { GET_ACTIVIDAD } from './types';
+import { CREATE_ACTIVIDAD, DELETE_ACTIVIDAD, EDIT_ACTIVIDAD, GET_ACTIVIDAD } from './types';
 
-import { getActivities } from '../../Services/actividadesService';
+import { deleteActivities, getActivities, postActivities, putActivities } from '../../Services/actividadesService';
+import { ACTION_FAILED } from '../novedades/types';
 
-export const GET_ACTIVIDAD_FUNCTION = (id) => (distpatch) => {
+export const GET_ACTIVIDAD_FUNCTION = (id = null) => (dispatch) => {
   try {
-    const response = getActivities(process.env.REACT_APP_API_ACTIVITIES, id);
-    console.log('response-action:', response);
-    distpatch({
+    const actividades = getActivities(process.env.REACT_APP_API_ACTIVITIES, id);
+    console.log('actividades-action:', actividades);
+    dispatch({
       type: GET_ACTIVIDAD,
+      payload: actividades,
+    });
+  } catch (error) {
+    dispatch({
+      type: ACTION_FAILED,
+      payload: error.message,
+    });
+  }
+};
+
+export const EDIT_ACTIVIDAD_FUNCTION = (id, data) => (dispatch) => {
+  try {
+    const response = putActivities(id, data);
+    dispatch({
+      type: EDIT_ACTIVIDAD,
       payload: response,
     });
   } catch (error) {
+    dispatch({
+      type: ACTION_FAILED,
+      payload: error.message,
+    });
+  }
+};
 
+export const CREATE_ACTIVIDAD_FUNCTION = (id, data) => (dispatch) => {
+  try {
+    const response = postActivities(id, data);
+    dispatch({
+      type: CREATE_ACTIVIDAD,
+      payload: response,
+    });
+  } catch (error) {
+    dispatch({
+      type: ACTION_FAILED,
+      payload: error.message,
+    });
+  }
+};
+
+export const DELETE_ACTIVIDAD_FUNCTION = (id) => async (dispatch) => {
+  try {
+    await deleteActivities(id);
+    dispatch({
+      type: DELETE_ACTIVIDAD,
+      payload: { id },
+    });
+  } catch (error) {
+    dispatch({
+      type: ACTION_FAILED,
+      payload: error.message,
+    });
   }
 };
