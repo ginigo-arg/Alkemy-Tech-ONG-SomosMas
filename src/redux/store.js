c6666316008472e8c036b93937c1e8038e15d07f
Duhan Renteria <derh7280@hotmail.com>
2022-04-15 19:44:10 -0500

    Redux categories

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import actividadesReducer from './actividades/reducer';
 import novedadesReducer from './novedades/reducer';
+import { categoriesReducer } from './categories/reducer';
 
 export const store = createStore(
   combineReducers({
     novedades: novedadesReducer,
     actividades: actividadesReducer,
+    categories: categoriesReducer,
     // ...other reducers
   }),
   applyMiddleware(thunk),
