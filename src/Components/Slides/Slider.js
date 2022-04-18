cfb1a3e9948924f9465258da9e5a55ef0b13a729
ginigo <gabrielinigo99@gmail.com>
2022-04-17 23:37:33 -0300

    mejorando slider

--- src/Components/Slides/Slider.js
+++ src/Components/Slides/Slider.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Carousel, CarouselItem } from 'react-bootstrap';
+import { Carousel, CarouselItem, Row, Col, Button } from 'react-bootstrap';
+import ParserHtml from '../Parser/Parser';
 import './Slider.css';
+import { IoPlayCircle } from 'react-icons/io5';
 
 const Slider = ({ slides }) => {
   return (
-    <Carousel className = "mb-5">
+    <Carousel className = "mb-5 carrusel">
       {slides &&
         slides.map((slide) => (
-          <CarouselItem interval={5000} key={slide.name}>
+          <CarouselItem interval={5000} key={slide.name} className='carouselItem carousel-fade'>
             <img src={slide.image} alt={slide.name} className="w-100 slides-img" />
-            <Carousel.Caption className="caption">
-              <h2 className="fw-bold text-uppercase fs-1">{slide.name}</h2>
-              <p>{slide.description}</p>
+            <Carousel.Caption className="caption carousel-fade">
+
+              <h2 className="fw-bold text-uppercase fs-1 text-start">{slide.name}</h2>
+              <ParserHtml
+                className='text-start'
+                text={slide.description} />
+              <Row className='mt-2 w-25'>
+                <Col>
+                  <Button varian='warning' className='text-white'>
+                    Ayudar
+                  </Button>
+                </Col>
+                <Col>
+                  <Button className='btn btn-warning text-white w-100'>
+                    <IoPlayCircle className='icon' fontSize="25px" color="grey" />
+                    Ver Video
+                  </Button>
+                </Col>
+              </Row>
+
             </Carousel.Caption>
           </CarouselItem>
         ))}
