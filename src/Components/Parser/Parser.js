a2e52f7dae6e404f7fc3f5db5c8f634500086064
ginigo <gabrielinigo99@gmail.com>
2022-04-18 11:36:14 -0300

    Corrigiendo parser

--- src/Components/Parser/Parser.js
+++ src/Components/Parser/Parser.js
@@ -3,7 +3,10 @@ import parse from 'html-react-parser';
 const ParserHtml = ({ text }) => {
   return (
     <>
-      {parse(`${text}`)}
+      <p>
+
+        {parse(`${text}`)}
+      </p>
     </>
   );
 };
