f4bb73b81237a68ae0bf6b99769bde5b959a24ec
Horuuuu <h2.globo@gmail.com>
2022-04-18 12:46:52 -0300

    cambios logica boton

--- src/Components/Auth/LogButton.js
+++ src/Components/Auth/LogButton.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
 const token = localStorage.getItem('token');
 
 const LogButton = () => {
   return (
     <div>{!token
       ? (
-        <Button onClick={'/login'}>Login</Button>
+        <Button><Link to='/login'></Link>Login</Button>
       )
       : (
-        <Button onClick={'/'}>Logout</Button>
+        <Button><Link to='/login'></Link>Logout</Button>
       )}</div>
   );
 };
