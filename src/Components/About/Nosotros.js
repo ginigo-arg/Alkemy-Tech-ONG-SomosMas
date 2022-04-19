861fe51641e579cdc1e061b1a53d7be8d2c8738c
ginigo <gabrielinigo99@gmail.com>
2022-04-19 13:37:42 -0300

    Pagina nosotros

--- src/Components/About/Nosotros.js
+++ src/Components/About/Nosotros.js
@@ -1,44 +1,55 @@
 import { useEffect, useState } from 'react';
 import { Container, Spinner } from 'react-bootstrap';
-import Title from '../SectionTitles/SectionTitles';
-import Card from '../Card/CardNosotros';
+// import Title from '../SectionTitles/SectionTitles';
 import { getAllMembers } from '../../Services/MemberService';
 import { alertService } from '../../Services/alertService';
+import CardMembers from './cardTeam';
 
 export default function Nosotros () {
-  const nosotrostx = 'Sobre Nosotros';
   const [members, setMembers] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(async () => {
-    await getAllMembers()
-      .then(res => setMembers(res))
+    const { data } = await getAllMembers()
       .catch(err => alertService('error', err))
       .finally(setLoading(true));
-  }, []);
+    setMembers(data);
+    console.log('members:', data);
+  }, [setMembers]);
 
   return (
     !loading
       ? <Spinner />
       : <>
-        <Title
-          title={nosotrostx}
-          backgroundImg="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhW6bkouhOtOGiBmCAT9T27gOEbBFD-5utzvSKrS-NPZUlMPEA4zNPYqZZmubU-AQ3nQM&usqp=CAU"
-        />
-        <Container>
-          <ul className="my-5 p-0 d-flex flex-wrap">
+        <Container className='mt-5'>
+          <h1 className='text-primary mb-3'>
+            <strong>
+              Una organización sin fines de lucro para niños que necesitan de nuestra ayuda
+            </strong>
+          </h1>
+          <p>
+            Tatarena es una organización sin fines de lucro que busca conectar personas y empresas a proyectos independientes, capaces de transformar vidas. Nacimos con un gran propósito: dar visibilidad a los proyectos que necesitan ser vistos, estén donde estén.
+
+            A través de nuestra plataforma, diseñada y desarrollada por expertos, así como estrategias de contenido digital, estamos utilizando la tecnología y lo mejor para impactar a las sociedades.
+          </p>
+
+          <h2 className='text-center text-primary'>
+            <strong>
+              Nuestro equipo
+            </strong>
+          </h2>
+          <Container className="d-flex gap-4 justify-content-center align-items-stretch flex-wrap mt-5 mb-5">
             { members.length > 0
               ? (
-                members.map((element) => {
+                members.map((item) => {
                   return (
-                    <Card
-                      key={element.title}
-                      name={element.name}
-                      description={element.description}
-                      image={element.image}
-                      facebookUrl={element.facebookUrl}
-                      linkedinUrl= {element.linkedinUrl}
-
+                    <CardMembers
+                      key={item.id}
+                      name={item.name}
+                      description={item.description}
+                      image={item.image}
+                      facebookUrl={item.facebookUrl}
+                      linkedingUrl= {item.linkedingUrl}
                     />
                   );
                 })
@@ -46,7 +57,8 @@ export default function Nosotros () {
               : (
                 <p className="w-100 my-5 text-center display-6">No hay Miembros</p>
               ) }
-          </ul>
+          </Container>
+
         </Container>
       </>
   );
