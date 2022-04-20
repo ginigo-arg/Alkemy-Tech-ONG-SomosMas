593b7540f54ccaaff3994f4b2955476c0d4dbe5c
Duhan Renteria <derh7280@hotmail.com>
2022-04-20 13:01:52 -0500

    Implementando authService

--- src/redux/auth/authReducer.js
+++ src/redux/auth/authReducer.js
@@ -1,36 +1,72 @@
-import { LOGIN_USER, LOGOUT_USER, CREATE_USER, LOGIN_USER_FAILED, CREATE_USER_FAILED } from './authTypes';
+import { LOGIN_USER, LOGOUT_USER, CREATE_USER, LOGIN_USER_FAILED, CREATE_USER_FAILED, LOGIN_AUTH } from './authTypes';
 import { alertService } from '../../Services/alertService';
 
 const initialState = {
   auth: false,
+  token: '',
+  user: '',
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_USER:
-      return {
-        auth: action.payload,
-      };
+    {
+      // console.log('soy el reducer LOGIN', action.payload);
+      return action.payload
+        ? {
+          ...state,
+          auth: true,
+          token: action.payload.token,
+          user: action.payload.user,
+        }
+        : { state };
+    }
 
-    case LOGOUT_USER:
+    case LOGIN_AUTH:
+    {
+      // console.log('soy el reducer AUTH', action.payload);
+      return action.payload
+        ? {
+          ...state,
+          auth: true,
+          token: action.token,
+          user: action.payload.user,
+        }
+        : { state };
+    }
+
+    case LOGOUT_USER: {
       localStorage.clear();
       return {
-        auth: initialState,
+        // auth: initialState,
+        state,
       };
+    }
 
-    case CREATE_USER:
-      return {
-        auth: action.payload,
-      };
+    case CREATE_USER: {
+      // console.log('soy el reducer CREATE', action.payload);
+      return action.payload
+        ? {
+          ...state,
+          auth: false,
+          token: action.payload.token,
+          user: action.payload.user,
+        }
+        : { state };
+    }
 
-    case LOGIN_USER_FAILED:
+    case LOGIN_USER_FAILED: {
       return alertService('error', action.payload);
+    }
 
     case CREATE_USER_FAILED:
+    {
       return alertService('error', action.payload);
+    }
 
-    default:
+    default: {
       return state;
+    }
   }
 };
 
