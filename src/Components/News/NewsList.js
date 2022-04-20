d11f6bd263294eab1949e723802dd021ebc03493
Nicolás Corradini <nicocorradini471@gmail.com>
2022-04-19 22:20:14 -0300

    Fixed new list

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,12 +1,11 @@
 import '../CardListStyles.css';
-import { Container, Row, Col, CardGroup } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import SectionTitles from '../SectionTitles/SectionTitles';
 import { useState, useEffect } from 'react';
 import ProgressSpinner from '../Progress/ProgressSpinner';
 import { getNews } from '../../Services/NewsService';
 import { alertService } from '../../Services/alertService';
-// import NewCard from './NewsCard';
-import CardComponent from '../Card/Card';
+import NewsCard from './NewsCard';
 
 const NewsList = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -31,33 +30,13 @@ const NewsList = () => {
         <ProgressSpinner state={isLoading} />
         {
           news.length > 0 && news.map((item) => (
-            <NewCard key={item.id}
+            <NewsCard key={item.id}
               image={item.image}
               title={item.name}
               description={item.content}
               id={item.id}
             />
           ))
-        }
-        {isLoading
-          ? <div className='h-100 d-flex justify-content-center align-items-center fw-5'>
-            <ProgressSpinner state={isLoading} dimention={10} />
-          </div>
-          : (
-            <Row className="g-2">
-              <CardGroup>
-                {
-                  news
-                    ? news.map((novedad, index) => (
-                      <Col md={4} key={index}>
-                        <CardComponent image={novedad.image} title={novedad.name} description={novedad.content} footerInformation={`<a href='./novedades/${novedad.id}'>Ver detalles</a>`} />
-                      </Col>
-                    ))
-                    : <p>No hay noticias para visualizar</p>
-                }
-              </CardGroup>
-            </Row>
-          )
         }
       </Container>
     </>
