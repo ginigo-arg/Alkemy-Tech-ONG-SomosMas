132bf98d678369684fd15090c38d164fc67063f6
Duhan Renteria <derh7280@hotmail.com>
2022-04-20 18:50:08 -0500

    Creando check, get, create y remove token funcion

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,5 +1,26 @@
 import { Get, Post } from './publicApiService';
 import { alertService } from './alertService';
+import { CHECK_IN_LOCAL_STORAGE, CREATE_IN_LOCAL_STORAGE, GET_FROM_LOCAL_STORAGE, REMOVE_FROM_LOCAL_STORAGE } from './localStorageService';
+
+export const CHECK_TOKEN = () => {
+  return CHECK_IN_LOCAL_STORAGE('TOKEN');
+};
+
+export const GET_TOKEN = () => {
+  if (CHECK_TOKEN) { return JSON.stringify(GET_FROM_LOCAL_STORAGE('TOKEN')); } else { return ''; };
+};
+
+export const CREATE_TOKEN = (token) => {
+  if (token) {
+    CREATE_IN_LOCAL_STORAGE('TOKEN', JSON.stringify(token));
+  } else {
+    alertService('error', 'No se pudo crear el token, se requiere un valor.');
+  }
+};
+
+export const REMOVE_TOKEN = () => {
+  REMOVE_FROM_LOCAL_STORAGE('TOKEN');
+};
 
 export const REGISTER = async (data) => {
   const response = await Post(process.env.REACT_APP_API_AUTH_REGISTER, data);
