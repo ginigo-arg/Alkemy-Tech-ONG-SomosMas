e2f56eede22503cb874823603abd4b283d7ed80c
ginigo <gabrielinigo99@gmail.com>
2022-04-19 21:19:17 -0300

    Modificando secciones

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -3,30 +3,16 @@ import { Container } from 'react-bootstrap';
 import SectionTitles from '../SectionTitles/SectionTitles';
 import { useState, useEffect } from 'react';
 import ProgressSpinner from '../Progress/ProgressSpinner';
-// import { getNews } from '../../Services/NewsService';
-// import { alertService } from '../../Services/alertService';
 import NewCard from './NewsCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { GET_NOVEDAD_FN } from '../../redux/novedades/actions';
 
 const NewsList = () => {
   const [isLoading, setIsLoading] = useState(true);
-  // const [news, setNews] = useState([]);
   const dispatch = useDispatch();
   const state = useSelector(state => state.novedades);
   console.log('state:', state);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const data = await getNews();
-  //     setIsLoading(false);
-  //     setNews(data);
-  //   };
-  //   fetchData().catch((e) => {
-  //     alertService('error', e.message);
-  //     setIsLoading(false);
-  //   });
-  // }, []);
   useEffect(() => {
     dispatch(GET_NOVEDAD_FN());
     if (state.length > 0) setIsLoading(false);
