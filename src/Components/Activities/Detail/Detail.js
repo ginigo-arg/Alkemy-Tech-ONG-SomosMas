b12ae5a775a48fd347a80e5fadc2f05209362f1b
Nicolás Corradini <nicocorradini471@gmail.com>
2022-04-22 13:24:00 -0300

    Fixed

--- src/Components/Activities/Detail/Detail.js
+++ src/Components/Activities/Detail/Detail.js
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Alert } from 'react-bootstrap';
+import React, { useState, useEffect, Suspense } from 'react';
+import { Container, Alert, Card } from 'react-bootstrap';
 import { getActivities } from '../../../Services/actividadesService';
 import { alertService } from '../../../Services/alertService';
 import { useParams } from 'react-router-dom';
 import ProgressSpinner from '../../Progress/ProgressSpinner';
 import LazyImg from '../../Lazyload/LazyImg';
+import { useScreen } from '../../../hooks/useScreen';
+import Comments from '../../News/Detail/Comments';
 import ParserHtml from '../../Parser/Parser';
 
 const Detail = (props) => {
-  // const [activityDetail, setActivityDetail] = useState(false);
   const { id } = useParams();
+  const { isScreen, fromRef } = useScreen();
 
   const [activityDetail, setActivityDetail] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -40,13 +42,26 @@ const Detail = (props) => {
           : <>
             {
               activityDetail.id
-                ? <>
-                  <div>
-                    <LazyImg image={activityDetail.image} alt='Imagen de la novedad' />
-                  </div>
-                  <div className='mt-5 px-2 h-100 w-100' style={{ textAlign: 'justify' }}>
-                    <ParserHtml text={activityDetail.description} />
-                  </div></>
+                ? <Container className="d-flex justify-content-center flex-wrap my-5">
+                  <Card className='news-container p-lg-5 d-flex flex-column align-items-center'>
+                    <div className='news-image-container'>
+                      <LazyImg image={activityDetail.image} alt="Imagen de la novedad" />
+                    </div>
+                    <Card.Body>
+                      <Card.Title className='news-title'>{`${activityDetail.name}`}</Card.Title>
+                    </Card.Body>
+                    <Card.Body>
+                      <div ref={fromRef} className="w-100">
+                        <div style={{ minHeight: '200px' }}>
+                          <ParserHtml text={activityDetail.description} />
+                        </div>
+                        <Suspense>
+                          { isScreen ? <Comments /> : null }
+                        </Suspense>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Container>
                 : <Alert key={'alert_actividad_' + id} className='h-100 w-100 fs-1 bg-light d-flex justify-content-center align-items-center' style={{ minHeight: '300px' }} variant='light'>
                   <div>
                     {msnError + ' '}
